ecfa3f6f5074fbc54c8ba89ce001f3d4e17edc35
202-04\User <toma1019@gmail.com>
2022-01-18 17:47:57 +0900

    DB 연동

---
+++ src/main/java/com/example/lesson02/UsedGoodsRestController.java
@@ -0,0 +1,21 @@
+package com.example.lesson02;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson02.bo.UsedGoodsBO;
+import com.example.lesson02.model.UsedGoods;
+
+@RestController
+public class UsedGoodsRestController {
+	@Autowired
+	private UsedGoodsBO usedGoodsBO; // 객체선언  
+	
+	@RequestMapping("/lesson02/ex01")
+	public List<UsedGoods> ex01(){
+		return usedGoodsBO.getUsedGoodsList();
+	}
+}
